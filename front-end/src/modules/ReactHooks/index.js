bce958b6ae2936f604ba76f47c776858cddd92a3
Bryan Karta <karta.bryan@gmail.com>
2023-03-01 20:54:17 +0700

    Modify ReactHooks index
    use custom counterHooks

--- front-end/src/modules/ReactHooks/index.js
+++ front-end/src/modules/ReactHooks/index.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useCounter from './components/counterHooks';
+
+import userProfile from './json/userProfile.json';
 
 function ReactHooks() {
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        document.title = `you click ${count} times`
-    }, [count]);
-
+    const [count, increments, decrements] = useCounter(0, 1)
+    const { name, age } = userProfile;
     return (
         <div>
             <h2>React Hooks</h2>
-
+            <h4>{name}</h4>
+            <h4>{age}</h4>
             <p>you clicked {count}</p>
-            <button onClick={() => setCount(count + 1)}>
+            <button onClick={increments}>
                 increase count
+            </button>
+            <button onClick={decrements}>
+                decrease count
             </button>
         </div>
     )
