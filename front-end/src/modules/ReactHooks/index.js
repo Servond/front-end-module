d4b5dc140f56c2a9ca57125dc6176137ada3e578
Bryan Karta <karta.bryan@gmail.com>
2023-03-01 19:36:10 +0700

    Update ReactHooks component

--- front-end/src/modules/ReactHooks/index.js
+++ front-end/src/modules/ReactHooks/index.js
@@ -1,7 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
 function ReactHooks() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `you click ${count} times`
+    }, [count]);
+
     return (
         <div>
             <h2>React Hooks</h2>
+
+            <p>you clicked {count}</p>
+            <button onClick={() => setCount(count + 1)}>
+                increase count
+            </button>
         </div>
     )
 }
